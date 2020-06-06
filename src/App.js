37599e4dc6d2ed723f0ccf2e120375fb53a519aa
bhavadharani <bhavadharanks@gmail.com>
2020-06-06 11:19:42 +0530

    Added Footer Component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,64 @@
-import React from 'react';
-import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import NavBar from 'react-bootstrap/Navbar';
-import NavBrand from 'react-bootstrap/NavbarBrand';
-import Nav from 'react-bootstrap/Nav';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import NavBar from "react-bootstrap/Navbar";
+import NavBrand from "react-bootstrap/NavbarBrand";
+import Nav from "react-bootstrap/Nav";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./components/Footer";
 
 class App extends React.Component {
-  
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      title: 'Bhavadharani',
+      title: "Bhavadharani",
       headerLinks: [
-        { title: 'Home', path: '/'},
-        { title: 'About', path: '/about'},
-        { title: 'Contact', path: '/contact'}
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" },
       ],
       home: {
-        title: 'Be Infinite',
-        subTitle: 'Projects that make a difference',
-        text: 'Checkout my projects'
+        title: "Be Infinite",
+        subTitle: "Projects that make a difference",
+        text: "Checkout my projects",
       },
       about: {
-        title: 'About me'
+        title: "About me",
       },
       contact: {
-        title: 'Let\'s Talk'
-      }
-    }
+        title: "Let's Talk",
+      },
+    };
   }
-  render(){
-  return (
-    <Router>
-      <Container className="p-0" fluid={true}>
-        <NavBar className="border-bottom" bg="transparent" expand="lg">
-          <NavBar.Brand>Bhavadharani</NavBar.Brand>
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <NavBar className="border-bottom" bg="transparent" expand="lg">
+            <NavBar.Brand>Bhavadharani</NavBar.Brand>
 
-          <NavBar.Toggle className="border-0" aria-controls="navbar-toggle"/>
-          <NavBar.Collapse id="navbar-toggle">
-            <Nav className="ml-auto">
-              <Link className="nav-link" to="/">Home</Link>
-              <Link className="nav-link" to="/about">About</Link>
-              <Link className="nav-link" to="/contact">Contact</Link>
-
-            </Nav>
-          </NavBar.Collapse>
-        </NavBar>
-      </Container>
-    </Router>
-  );
-}
+            <NavBar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <NavBar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </Nav>
+            </NavBar.Collapse>
+          </NavBar>
+          <Footer />
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
